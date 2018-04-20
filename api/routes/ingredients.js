6e40c3d3641b06e891aaf3516712eb8b15e54456
Tyraael <anthonymarquet21@gmail.com>
2018-04-20 23:40:05 +0200

    Add monitoring package with morgan and nodemon. Add API Error Handling

--- api/routes/ingredients.js
+++ api/routes/ingredients.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Handle incoming GET request to /ingredients
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message : 'Get Ingredient Request'
--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const recipesRoutes = require('./api/routes/recipes');
 const ingredientsRoutes = require('./api/routes/ingredients');
 
+app.use(morgan('dev'));
 
+// Routes which should handle request
 app.use('/recipes', recipesRoutes);
 app.use('/ingredients', ingredientsRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
